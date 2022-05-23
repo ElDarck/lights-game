4eb2d3719d05eca9e4700cf0a05796832e57340d
ElDarck <evgeniy.titov013@gmail.com>
2022-05-23 19:42:19 +0300

    s

--- src/app/components/playground/playground.component.ts
+++ src/app/components/playground/playground.component.ts
@@ -10,15 +10,16 @@ export class PlaygroundComponent implements OnInit {
   allCells: any[] = [];
   usedCells: number[] = [];
 
-  gameSpeed: number = 0;
+  gameSpeed!: number;
   playerScore!: number;
   computerScore!: number;
-  currentCell: number = -1;
+  currentCell!: number;
 
   timeOut!: any;
 
   loading: boolean = false;
   inGame: boolean = false;
+  win: boolean = false;
 
   constructor() {
   }
@@ -33,35 +34,24 @@ export class PlaygroundComponent implements OnInit {
     }, 200);
   }
 
+  /*Начальные параметры*/
   initializePlayGround() {
+    this.win = false;
     this.playerScore = 0;
     this.computerScore = 0;
+
+    this.allCells = [];
+    this.usedCells = [];
+    this.gameSpeed = 0;
+    this.currentCell = -1;
 
     for( let i = 0; i < 100; i++) {
       let cell = { id: i, win: false, lose: false }
       this.allCells.push(cell);
     }
-
   }
 
-  updateInputValue(event:any) {
-    if (event.target.value < 1) {
-      this.gameSpeed = 0;
-      return;
-    }
-
-    this.gameSpeed = event.target.value;
-  }
-
-  cellClick ( event:any ) {
-    if (event.target.getAttribute("data-cell") == this.currentCell) {
-      this.allCells[this.currentCell].win = true;
-      this.playerScore++;
-      clearTimeout(this.timeOut);
-      this.choseCell();
-    }
-  }
-
+  /*Начало игры*/
   startGame () {
     this.inGame = true;
     if (this.gameSpeed <= 0) {
@@ -72,10 +62,11 @@ export class PlaygroundComponent implements OnInit {
     this.choseCell();
   }
 
+  /*Выбор клетки*/
   choseCell () {
-
     if (this.computerScore === 10 || this.playerScore === 10) {
       this.inGame = false;
+      this.win = true;
       return;
     }
 
@@ -90,23 +81,52 @@ export class PlaygroundComponent implements OnInit {
     }, this.gameSpeed)
   }
 
+  /*Генератор не повторяющихся чисел*/
   newCellNumber(cells:any[]): number {
     let randomNumber = Math.floor(Math.random() * this.allCells.length);
+
     let rValue: any = this.allCells[randomNumber];
+
     if (!this.compareNums(this.usedCells, rValue.id)) {
       this.usedCells.push(rValue);
-      console.log(rValue)
+
       return rValue.id;
     }
     return this.newCellNumber(cells);
   }
-
+  /*Сравнение повторяющихся чисел*/
   compareNums(usedNums: any[], currentElement:number ) {
     for (let i = 0; i < usedNums.length; i++ ) {
       if( usedNums[i].id === currentElement ) {
         return true;
       }
     }
+
     return false;
+  }
+
+  /*Задать значение времени*/
+  updateInputValue(event:any) {
+    if (event.target.value < 1) {
+      this.gameSpeed = 0;
+      return;
+    }
+
+    this.gameSpeed = event.target.value;
+  }
+
+  /*Выбор клетки*/
+  cellClick ( event:any ) {
+    if (event.target.getAttribute("data-cell") == this.currentCell) {
+      this.allCells[this.currentCell].win = true;
+      this.playerScore++;
+      clearTimeout(this.timeOut);
+      this.choseCell();
+    }
+  }
+
+  /*Рестарт игры*/
+  restart() {
+    this.initializePlayGround();
   }
 }
